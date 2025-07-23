51c8ef35bdd9af9eac95cc3df99b1e6b69a6f8e0
SandyWang <sandy20389@gmail.com>
2025-07-23 20:58:09 +0800

    0723下課

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("meowwww");
+
+// let a = 10
+// let b = 20
+// let temp = a
+// a = b
+// b = temp
+// console.log(a, b);
+
+// 邏輯運算子 == != && ||
+// let a = 10
+// let b = 20
+// console.log(a == b); // false
+// console.log(a != b); // true
+// console.log(a == 10 && b == 30); // false
+// console.log(a == 10 && b == 20); // true
+// console.log(a == 10 || b == 30); // true
+// console.log(a <= 10 || b >=20); // true
+
+// + - * / %
+// console.log(10 + 20); // 30
+// console.log(10 - 20); // -10
+// console.log(10 * 20); // 200
+// console.log(10 / 20); // 0.5
+// console.log(10 % 20); // 10
+
+// if (a == 10) {
+//   console.log("a is 10");
+// } else if (a == 20) {
+//     console.log("a is 20");
+// } else {
+//     console.log("a is neither 10 nor 20");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// console.log("for Loop End")
+
+let double = function (num) {
+    return num * 2;
+}
+let c = double(2);
+console.log(c); // 4
