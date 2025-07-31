94fe6fbf9a6334054f895b494121b0e5a95f47c4
SandyWang <sandy20389@gmail.com>
2025-07-31 22:03:03 +0800

    0731

--- index.js
+++ index.js
@@ -37,8 +37,36 @@ console.log("meowwww");
 // }
 // console.log("for Loop End")
 
-let double = function (num) {
-    return num * 2;
-}
-let c = double(2);
-console.log(c); // 4
+// let double = function (num) {
+//     return num * 2;
+// }
+// let c = double(2);
+// console.log(c); // 4
+
+// window.onload = function () {
+//     console.log(document.querySelector("#year"))
+//     document.querySelector("#year_btn").addEventListener("click", function () {
+//         const year = parseInt(document.querySelector("#year").value)
+//         console.log(year)
+//         let result
+//         if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//             result = year + " 年是閏年";
+//         } else {
+//             result = year + " 年是平年";
+//         }
+//         document.querySelector("#answer").innerHTML = result
+//     })
+// }
+
+// const isTriangle = (a, b, c) => {
+//  return (a > 0 && b > 0 && c > 0) && (a + b > c && a + c > b && b + c > a);
+// }
+// console.log(isTriangle(3, 4, 5))
+
+const countBMI = function (height, weight) {
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    return bmi.toFixed(2);
+};
+const bmi1 = countBMI(154, 46);
+console.log(`身高 175, 體重 70 的 BMI 是：${bmi1}`); 
